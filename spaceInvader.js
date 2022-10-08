29aa8ef921c65f16b8fa6c4a93dfefdb70e0d1f7
Bram Eylenbosch <bram.eylenbosch@student.ehb.be>
2022-10-08 10:28:51 +0200

    complete space invader

--- spaceInvader.js
+++ spaceInvader.js
@@ -1,3 +1,23 @@
 "use strict";
 
-alert("Draw your space invader here");
+let canvas = document.querySelector('canvas');
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+let context = canvas.getContext('2d');
+
+drawSpaceInvader();
+
+function drawSpaceInvader() {
+    context.beginPath();
+    context.fillRect(50, 50, 300, 300);
+    context.fillStyle = "black";
+
+    context.beginPath();
+    context.fillStyle = "lime";
+    context.fillRect(75, 75, 50, 250);
+    context.fillRect(275, 75, 50, 250);
+    context.fillRect(125, 275, 150, 50);
+    context.fillRect(125,175, 150, 50);
+    context.fillRect(125, 75, 50, 50);
+    context.fillRect(225, 75, 50 ,50);
+}
